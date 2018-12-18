1db1782735f495c46ee10d795c37bf3a55ba15a0
Heiko Piirme <heiko.piirme@gmail.com>
2018-12-18 16:32:42 +0200

    Basic producer and Rest controller

---
+++ src/main/java/com/heiko/votingapi/Web/WebController.java
@@ -0,0 +1,20 @@
+package com.heiko.votingapi.Web;
+
+import com.heiko.votingapi.Producer.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @Autowired
+    Producer producer;
+
+    @RequestMapping("/send")
+    public String sendMsg(@RequestParam("msg")String msg){
+        producer.produceMsg(msg);
+        return "Done";
+    }
+}
