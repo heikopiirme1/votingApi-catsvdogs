79e3aaf61eaa55e6683bb3bc1aa4d83a89df7d38
Heiko Piirme <heiko.piirme@gmail.com>
2018-12-18 16:20:47 +0200

    Spring Boot application created

---
+++ src/main/java/com/heiko/votingapi/VotingapiApplication.java
@@ -0,0 +1,14 @@
+package com.heiko.votingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingapiApplication.class, args);
+    }
+
+}
+
